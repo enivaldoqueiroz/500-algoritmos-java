b8bf1207c47531a2a306fc4b2ca502d6ede6c30a
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-06-23 23:23:20 -0300

    Ajuste algoritmo21

---
+++ src/algoritmos/resolvidos/Algoritmo21.java
@@ -0,0 +1,16 @@
+package algoritmos.resolvidos;
+
+public class Algoritmo21 {
+
+	public static void main(String[] args) {
+		// Algoritmo21
+		
+		System.out.println("\nTESTENDO HIERARQUIA\n");
+		System.out.println("\n12 + 5 / 2 È igual a: " + ((12+5)/2));
+		System.out.println("\n… DIFERENTE DE (12 + 5)/2 QUE … IGUAL A: " + ((12+5)/2) + " logo / tem HIERARQUIA MAIOR do que + ou -");//Continuacao
+		float x = 1/4;
+		System.out.println("\n\n64^1/4 È igual a: " + Math.pow(64,1/4));
+		
+	}
+
+}
