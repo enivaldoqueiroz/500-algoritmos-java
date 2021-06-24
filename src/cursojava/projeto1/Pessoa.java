58813ca5cad5fe4130520601c7ccc6d3c8033249
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-06-23 23:42:11 -0300

    Implementando cursojava.projeto1

---
+++ src/cursojava/projeto1/Pessoa.java
@@ -0,0 +1,29 @@
+package cursojava.projeto1;
+
+public class Pessoa {
+	
+		private String nome;
+		private String sobrenome;
+		public String getNome() {
+			return nome;
+		}
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+		public String getSobrenome() {
+			return sobrenome;
+		}
+		public void setSobrenome(String sobrenome) {
+			this.sobrenome = sobrenome;
+		}
+		public int getIdade() {
+			return idade;
+		}
+		public void setIdade(int idade) {
+			this.idade = idade;
+		}
+		private int idade;
+
+	
+
+}
