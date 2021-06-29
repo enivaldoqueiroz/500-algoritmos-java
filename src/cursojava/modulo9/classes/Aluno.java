f697b15c10c1836c8b6bbda8d344ae6dc73c5d62
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-06-28 22:53:59 -0300

    Primeiro método de calculo de média de nota

--- src/cursojava/modulo9/classes/Aluno.java
+++ src/cursojava/modulo9/classes/Aluno.java
@@ -16,6 +16,14 @@ public class Aluno {
 	private String nomeEscola;
 	private String serieMatriculado;
 	
+	// TODO Modulo 9 - Aula9.11 - Primeiro método de calculo de média de nota - Inicio
+	private double nota1;
+	private double nota2;
+	private double nota3;
+	private double nota4;
+			
+	// TODO Modulo 9 - Aula9.11 - Primeiro método de calculo de média de nota - Fim
+	
 	public Aluno() {
 		// TODO Cria os dados na memoria - Sendo padrão do Java
 	}
@@ -114,6 +122,43 @@ public class Aluno {
 		return this.idade;
 	}
 	
+	// TODO Modulo 9 - Aula9.11 - Primeiro método de calculo de média de nota - Inicio
+	public double getNota1() {
+		return nota1;
+	}
 	
+	public void setNota1(double nota1) {
+		this.nota1 = nota1;
+	}
+
+	public double getNota2() {
+		return nota2;
+	}
+
+	public void setNota2(double nota2) {
+		this.nota2 = nota2;
+	}
+
+	public double getNota3() {
+		return nota3;
+	}
+
+	public void setNota3(double nota3) {
+		this.nota3 = nota3;
+	}
+
+	public double getNota4() {
+		return nota4;
+	}
+
+	public void setNota4(double nota4) {
+		this.nota4 = nota4;
+	}
 	
+	//Metodos
+	
+	public double getMediaNota() {
+		return (nota1 + nota2 + nota3 + nota4)/4;
+	}
+	// TODO Modulo 9 - Aula9.11 - Primeiro método de calculo de média de nota - Fim
 }
