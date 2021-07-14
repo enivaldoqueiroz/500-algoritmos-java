2bb6f1d7d4ac27f926b4e6b3bbe781cd718de4d7
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-07-14 10:04:56 -0300

    13.2 - Criando nosso primeiro Array

---
+++ src/cursojava/modulo13/arrayematriz/ArrayVetor.java
@@ -0,0 +1,22 @@
+package cursojava.modulo13.arrayematriz;
+
+public class ArrayVetor {
+	
+	/*Usado para executar código*/
+	public static void main(String[] args) {
+		
+		/*TODO Array pode ser de todos o tipos de dados e objetos também*/
+		
+		/*Array sem deve ter a quantidade de posições definidas*/
+		double[] notas = new double[3];
+		
+		/*Atribuir valores na posição do array*/
+		notas[0] = 7.8;
+		notas[1] = 8.7;
+		notas[2] = 9.7;
+		notas[3] = 6.0;
+		
+		System.out.println("");
+	}
+
+}
