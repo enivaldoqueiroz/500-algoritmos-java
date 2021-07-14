caddc903d8a5f61f3c27e97f877b32b48166af7a
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-07-14 10:53:41 -0300

    13.4 - Entrada e leitura de forma dinâmica

--- src/cursojava/modulo13/arrayematriz/ArrayVetor.java
+++ src/cursojava/modulo13/arrayematriz/ArrayVetor.java
@@ -1,5 +1,9 @@
 package cursojava.modulo13.arrayematriz;
 
+import java.util.Iterator;
+
+import javax.swing.JOptionPane;
+
 public class ArrayVetor {
 	
 	/*Usado para executar código*/
@@ -7,23 +11,37 @@ public class ArrayVetor {
 		
 		/*TODO Array pode ser de todos o tipos de dados e objetos também*/
 		
+		String posicaes = JOptionPane.showInputDialog("Quantas posições o array deve ter?");
+		
 		/*Array sem deve ter a quantidade de posições definidas*/
-		double[] notas = new double[5];
+		double[] notas = new double[Integer.parseInt(posicaes)];
 		
-		/*Atribuir valores na posição do array*/
-		notas[0] = 7.8;
-		notas[1] = 8.7;
-		notas[2] = 9.7;
-		notas[3] = 6.0;
-		
-		/*System.out.println("nota 1 " + notas[0]);
-		System.out.println("nota 2 " + notas[1]);
-		System.out.println("nota 3 " + notas[2]);
-		System.out.println("nota 4 " + notas[3]);*/
+		for (int pos = 0; pos < notas.length; pos++) {
+			String valor = JOptionPane.showInputDialog("Qual o valor da posição = " + (pos + 1));
+			notas[pos] = Double.valueOf(valor);
+		}
 		
 		for(int pos = 0; pos <notas.length; pos++) {
 			System.out.println("Nota " + (pos + 1) + " é = " + notas[pos]);
 		}
+		
+		/*Atribuir valores na posição do array*/
+		/*notas[0] = 7.8;
+		notas[1] = 8.7;
+		notas[2] = 9.7;
+		notas[3] = 6.0;*/
+		
+		/*System.out.println("nota 1 " + notas[0]);
+		System.out.println("nota 2 " + notas[1]);
+		System.out.println("nota 3 " + notas[2]);
+		System.out.println("nota 4 " + notas[3]);
+		
+		for(int pos = 0; pos <notas.length; pos++) {
+			System.out.println("Nota " + (pos + 1) + " é = " + notas[pos]);
+		}
+		*/
+		
+		
 	}
 
 }
