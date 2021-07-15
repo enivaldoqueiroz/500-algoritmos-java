bb95854a6efe8430b97722e5e1b92c74fd153091
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-07-15 09:28:23 -0300

    13.12 - Criando uma Matriz multidimensional

---
+++ src/cursojava/modulo13/arrayematriz/Matriz.java
@@ -0,0 +1,23 @@
+package cursojava.modulo13.arrayematriz;
+
+public class Matriz {
+
+	public static void main(String[] args) {
+		// TODO Matriz
+		
+		int notas[][] = new int[2][3];
+		
+		notas[0][0] = 80;
+		notas[0][1] = 90;
+		notas[0][2] = 70;
+		
+		notas[1][0] = 40;
+		notas[1][1] = 60;
+		notas[1][2] = 30;
+		
+		
+		System.out.println(notas[0][0]);
+
+	}
+
+}
