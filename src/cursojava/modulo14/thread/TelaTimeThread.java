69aa18f48a957ee93197f4fe1aafd3c16f8cf8ee
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-07-16 18:11:22 -0300

    14.7 - Conhecendo o GridBagConstraints gerenciador de posicionamento e layout

--- src/cursojava/modulo14/thread/TelaTimeThread.java
+++ src/cursojava/modulo14/thread/TelaTimeThread.java
@@ -1,17 +1,52 @@
 package cursojava.modulo14.thread;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
 import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class TelaTimeThread extends JDialog {
+	
+	private JPanel jPanel = new JPanel(new GridBagLayout());//Painel de componentes
+	
+	private JLabel descricaoHora = new JLabel("Time Thread 1");
+	private JTextField mostraTempo = new JTextField();
+	
+	private JLabel descricaoHora2 = new JLabel("Time Thread 2");
+	private JTextField mostraTempo2 = new JTextField();
 
 	public TelaTimeThread() {// Executa o que estiver dentro no momento da abertura ou execução
-		setTitle("Tela de time com Thread");
+		setTitle("Minha Tela de time com Thread");
 		setSize(new Dimension(240,240));
 		setDefaultLookAndFeelDecorated(rootPaneCheckingEnabled);
 		setResizable(false);
+		//Primeira parte concluida
 		
+		GridBagConstraints gridBagConstraints = new GridBagConstraints();//Controlador de posicionamento de componentes
+		gridBagConstraints.gridx = 0;
+		gridBagConstraints.gridy = 0;
+		
+		descricaoHora.setPreferredSize(new Dimension(200, 25));
+		jPanel.add(descricaoHora,gridBagConstraints);
+		
+		mostraTempo.setPreferredSize(new Dimension(200, 25));
+		gridBagConstraints.gridy ++;
+		jPanel.add(mostraTempo, gridBagConstraints);
+		
+		descricaoHora2.setPreferredSize(new Dimension(200, 25));
+		gridBagConstraints.gridy ++;
+		jPanel.add(descricaoHora2,gridBagConstraints);
+		
+		mostraTempo2.setPreferredSize(new Dimension(200, 25));
+		gridBagConstraints.gridy ++;
+		jPanel.add(mostraTempo2, gridBagConstraints);
+		
+		add(jPanel, BorderLayout.WEST);
 		
 		setVisible(true);//Torna a tela visivel para o usuário
 	}
