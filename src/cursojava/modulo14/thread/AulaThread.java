a350c262b016c66037d7e949ece2cc010a350885
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-07-16 12:48:03 -0300

    14.3 - Criando a primeira Thread

--- src/cursojava/modulo14/thread/AulaThread.java
+++ src/cursojava/modulo14/thread/AulaThread.java
@@ -1,17 +1,38 @@
 package cursojava.modulo14.thread;
 
+import javax.swing.JOptionPane;
+
 public class AulaThread {
 
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Thread
 
-		for (int pos = 0; pos < 10; pos++) {
-			
-			//Executar com o tempo de parada determinado
-			System.out.println("Executando alguma rotina, por exemplo envio de email");
-			Thread.sleep(1000);//Dar um tempo
+		//Thread processando em paralelo
+		new Thread() {
+			public void run() {//Executa o que nós queremos
+				//Codigo da rotina
+				for (int pos = 0; pos < 10; pos++) {
+					
+					//Executar com o tempo de parada determinado
+					System.out.println("Executando alguma rotina, por exemplo envio de email");
+					//JOptionPane.showMessageDialog(null, "Sistema continua executando para o usuário");
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}//Dar um tempo
 
-		}
+				}
+				//Fim do codigo paralelo
+			};
+		}.start();//Start inicia a thread que fica processando paralelamente por trás
+		
+		
+		
+		//Codigo do sistema do usuario continua o fluxo de trabalho
 		System.out.println("Fim do codigo de Thread");
+		//Fluxo do sistema, cadastro de vendas
+		JOptionPane.showMessageDialog(null, "Sistema continua executando para o usuário");
 	}
 }
