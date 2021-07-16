8ef7a24ac2f4140f6b7bbd20a0b55f4974997854
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-07-16 14:47:36 -0300

    14.5 - Thread em objeto e o Runnable

--- src/cursojava/modulo14/thread/AulaThread.java
+++ src/cursojava/modulo14/thread/AulaThread.java
@@ -8,53 +8,64 @@ public class AulaThread {
 		// TODO Thread
 
 		//Thread processando em paralelo do envio de email
-		new Thread() {
-			public void run() {//Executa o que nós queremos
-				//Codigo da rotina
-				for (int pos = 0; pos < 10; pos++) {
-					
-					//Executar com o tempo de parada determinado
-					System.out.println("Executando alguma rotina, por exemplo envio de email");
-					//JOptionPane.showMessageDialog(null, "Sistema continua executando para o usuário");
-					try {
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}//Dar um tempo
-
-				}
-				//Fim do codigo paralelo
-			};
-		}.start();//Start inicia a thread que fica processando paralelamente por trás
+		Thread threadEmail = new Thread(thread1);
+		threadEmail.start();
 		
 		//====================================================================================
 		
 		//Thread processando em paralelo do envio de nota fiscal
-		new Thread() {
-			public void run() {//Executa o que nós queremos
-				//Codigo da rotina
-				for (int pos = 0; pos < 10; pos++) {
+		 Thread threadNotaFiscal = new Thread(thread2);
+		 threadNotaFiscal.start();
 					
-					//Executar com o tempo de parada determinado
-					System.out.println("Executando alguma rotina, por exemplo envio de nota fiscal");
-					
-					try {
-						Thread.sleep(4000);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}//Dar um tempo
-
-				}
-				//Fim do codigo paralelo
-			};
-		}.start();//Start inicia a thread que fica processando paralelamente por trás
-		
-		
 		//Codigo do sistema do usuario continua o fluxo de trabalho
 		System.out.println("Fim do codigo de Thread");
 		//Fluxo do sistema, cadastro de vendas
 		JOptionPane.showMessageDialog(null, "Sistema continua executando para o usuário");
 	}
+	
+	private static Runnable thread1 = new Runnable() {
+		
+		@Override
+		public void run() {
+			
+			//Codigo da rotina
+			for (int pos = 0; pos < 10; pos++) {
+				
+				//Executar com o tempo de parada determinado
+				System.out.println("Executando alguma rotina, por exemplo envio de email");
+				//JOptionPane.showMessageDialog(null, "Sistema continua executando para o usuário");
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}//Dar um tempo
+
+			}
+			//Fim do codigo paralelo
+		}
+	};
+	
+	private static Runnable thread2  = new Runnable() {
+		
+		@Override
+		public void run() {
+			
+			//Codigo da rotina
+			for (int pos = 0; pos < 10; pos++) {
+				
+				//Executar com o tempo de parada determinado
+				System.out.println("Executando alguma rotina, por exemplo envio de nota fiscal");
+				
+				try {
+					Thread.sleep(4000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}//Dar um tempo
+
+			}
+			//Fim do codigo paralelo
+		}
+	};
 }
