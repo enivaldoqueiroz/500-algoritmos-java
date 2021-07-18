4e10cd1764f7e152228c1e869c66ea0c4d144173
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-07-18 20:49:42 -0300

    15.9 - Formatando data e hora com nova API

--- src/cursojava/modulo15/datas/NovaAPIDeDataNoJava.java
+++ src/cursojava/modulo15/datas/NovaAPIDeDataNoJava.java
@@ -4,6 +4,7 @@ import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class NovaAPIDeDataNoJava {
 
@@ -13,16 +14,21 @@ public class NovaAPIDeDataNoJava {
 		LocalDate dataAtual = LocalDate.now();
 		
 		System.out.println("Data atual : " + dataAtual);
+		System.out.println("Data atual formatada : " + dataAtual.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
 		
 		LocalTime horaAtual = LocalTime.now();
+		System.out.println("==============================================");
 		
 		System.out.println("Hora atual : " + horaAtual);
+		System.out.println("Hora atual formatada : " + horaAtual.format(DateTimeFormatter.ofPattern("HH:mm")));		
+		System.out.println("==============================================");
 		
 		LocalDateTime dataAtualHoraAtual = LocalDateTime.now();
 		
 		//System.out.println("Hora e Data : " + dataAtualHoraAtual);
-		System.out.println("Data e hora atual : " 
-		+ new SimpleDateFormat("dd-MM-yyyy").format(dataAtualHoraAtual.toLocalDate()));
+		System.out.println("Data e hora atual : " + dataAtualHoraAtual.format(DateTimeFormatter.ISO_DATE));
+		System.out.println("Data e hora atual : " + dataAtualHoraAtual.format(DateTimeFormatter.ISO_DATE_TIME));
+		System.out.println("Data e hora atual formatadaos: " + dataAtualHoraAtual.format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm")));
 		
 		
 	}
